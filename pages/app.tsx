a753763a6628514ab9521c6e204368ae73bf5efa
notnulldev <61083452+NotNullDev@users.noreply.github.com>
2023-01-13 23:22:42 +0100

    updated ui

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,8 +1,16 @@
-import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
+import {
+  Button,
+  FormControlLabel,
+  Paper,
+  Switch,
+  TextField,
+} from "@mui/material";
+import Link from "next/link";
 import { toast } from "react-toastify";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { EnvModal, openEnvModal } from "../components/EnvModal";
+import { NanoToolbar } from "../components/NanoToolbar";
 
 type AppStateStoreType = {
   envModalOpen: boolean;
@@ -24,70 +32,117 @@ const appStateStore = create<AppStateStoreType>()(
 
 const AppPage = () => {
   return (
-    <div className="flex flex-col gap-3 flex-1 p-6 items-center mt-12`">
-      <div className="flex flex-col gap-3 w-1/5">
-        <TextField id="standard-basic" label="App name" variant="standard" />
-        <FormControlLabel control={<Switch defaultChecked />} label="Enabled" />
-        <TextField id="standard-basic" label="Repo url" variant="standard" />
-        <TextField
-          id="standard-basic"
-          label="Branch"
-          variant="standard"
-          placeholder="default"
-        />
-        <div className="flex gap-1 items-end">
-          <label className="whitespace-nowrap">Environment variables</label>
-          <Button
-            size="small"
-            onClick={() => {
-              openEnvModal({
-                title: "Environment variables",
-                onChange: (value) => {
-                  toast("Environment variables changed to " + value);
-                },
-                onCancel: () => {
-                  toast("Environment variables cancelled");
-                },
-                onSave: () => {
-                  toast("Environment variables saved");
-                },
-              });
-            }}
-          >
-            Edit
+    <div className="flex flex-col gap-3 flex-1 p-4 items-center`">
+      <NanoToolbar>
+        <div className="flex gap-2 items-center">
+          <Button className="text-yellow-500 h-min" variant="contained">
+            Download logs
           </Button>
         </div>
-        <div className="flex gap-1 items-end">
-          <label className="whitespace-nowrap">Build variables</label>
-          <Button
-            size="small"
-            onClick={() => {
-              openEnvModal({
-                title: "Build variables",
-                onChange: (value) => {
-                  toast("Build variables changed to " + value);
-                },
-                onCancel: () => {
-                  toast("Build variables cancelled");
-                },
-                onSave: () => {
-                  toast("Build variables saved");
-                },
-              });
-            }}
-          >
-            Edit
+        <div className="flex gap-2 items-center">
+          <Button className="text-yellow-500 h-min" variant="contained">
+            Build now
           </Button>
+        </div>
+        <div className="flex gap-2 items-center">
+          <Button
+            className="h-min text-orange-500"
+            color="error"
+            variant="text"
+          >
+            Delete app
+          </Button>
+        </div>
+      </NanoToolbar>
+      <div className="flex flex-1 container mx-auto justify-center gap-12">
+        <Paper className="w-1/5 flex flex-col gap-3 p-6 rounded-xl h-min">
+          <TextField id="standard-basic" label="App name" variant="standard" />
+          <FormControlLabel
+            control={<Switch defaultChecked />}
+            label="Enabled"
+          />
+          <TextField id="standard-basic" label="Repo url" variant="standard" />
           <TextField
             id="standard-basic"
-            label="Mount point"
+            label="Branch"
             variant="standard"
+            placeholder="default"
           />
-        </div>
-        <div className="flex gap-2 justify-end mt-5">
-          <Button variant="outlined">Save</Button>
-          <Button variant="outlined">Discard</Button>
-        </div>
+          <div className="flex gap-1 items-end items-center">
+            <label className="whitespace-nowrap">Environment variables</label>
+            <Button
+              size="small"
+              onClick={() => {
+                openEnvModal({
+                  title: "Environment variables",
+                  onChange: (value) => {
+                    toast("Environment variables changed to " + value);
+                  },
+                  onCancel: () => {
+                    toast("Environment variables cancelled");
+                  },
+                  onSave: () => {
+                    toast("Environment variables saved");
+                  },
+                });
+              }}
+            >
+              Edit
+            </Button>
+          </div>
+          <div className="flex gap-1 items-end">
+            <div className="flex items-center gap-2">
+              <label className="whitespace-nowrap">Build variables</label>
+              <Button
+                size="small"
+                onClick={() => {
+                  openEnvModal({
+                    title: "Build variables",
+                    onChange: (value) => {
+                      toast("Build variables changed to " + value);
+                    },
+                    onCancel: () => {
+                      toast("Build variables cancelled");
+                    },
+                    onSave: () => {
+                      toast("Build variables saved");
+                    },
+                  });
+                }}
+              >
+                Edit
+              </Button>
+            </div>
+            <TextField
+              id="standard-basic"
+              label="Mount point"
+              variant="standard"
+            />
+          </div>
+          <div className="flex gap-2 justify-end mt-5">
+            <Button
+              variant="text"
+              color="error"
+              onClick={() => {
+                toast("App saved");
+              }}
+            >
+              Save
+            </Button>
+            <Link href="/">
+              <Button variant="outlined" className="text-yellow-500">
+                Discard
+              </Button>
+            </Link>
+          </div>
+        </Paper>
+        <Paper className="h-min px-4 rounded-xl" elevation={4}>
+          <Paper className="p-4 rounded-xl h-80 w-[45vw] mt-4"></Paper>
+          <div className="p-4 justify-end w-full flex">
+            <Button>Refetch logs</Button>
+            <Button>Download logs</Button>
+          </div>
+        </Paper>
       </div>
       <EnvModal />
     </div>
--- pages/management.tsx
+++ pages/management.tsx
@@ -1,12 +1,23 @@
-import { Button, FormControl, Input, InputLabel } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  Paper,
+} from "@mui/material";
+import { useState } from "react";
 import { toast } from "react-toastify";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 import { NanoToolbar } from "../components/NanoToolbar";
 
 const ManagementPage = () => {
   return (
     <div className="flex-1 flex flex-col p-4">
       <NanoToolbar>
-        <div></div>
         <div className="flex items-center gap-2">
           <Button variant="outlined" className="text-yellow-500">
             Copy token
@@ -16,6 +27,21 @@ const ManagementPage = () => {
           </Button>
         </div>
       </NanoToolbar>
+      <div className="flex flex-1 justify-center gap-4">
+        <Paper className="p-6 rounded-xl flex flex-col gap-3 w-min h-96">
+          <div className="text-xl">Login</div>
+          <UsernameInput />
+          <PasswordInput />
+          <Button>Update password</Button>
+        </Paper>
+
+        <Paper className="flex flex-col gap-2 p-6 rounded-xl h-96">
+          <div className="text-xl">Metadata</div>
+          <NewTokenInput />
+          <ServerUrlInput />
+          <Button>Update metadata</Button>
+        </Paper>
+      </div>
     </div>
   );
 };
@@ -30,12 +56,145 @@ function NewTokenInput() {
       }}
       variant="standard"
     >
-      <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+      <InputLabel htmlFor="standard-adornment-password">New token</InputLabel>
       <Input
-        id="standard-adornment-password"
+        id="standard-adornment-new-token"
         type={"text"}
         value={val}
         onChange={(e) => toast("e")}
+      />
+    </FormControl>
+  );
+}
+
+type LoginStoreType = {
+  username: string;
+  password: string;
+};
+
+const loginStore = create<LoginStoreType>()(
+  immer((set) => {
+    return {
+      username: "",
+      password: "",
+    };
+  })
+);
+
+function PasswordInput() {
+  const [showPassword, setShowPassword] = useState(false);
+  const password = loginStore((state) => state.password);
+  return (
+    <>
+      <FormControl
+        sx={{
+          m: 1,
+          width: "25ch",
+        }}
+        variant="standard"
+      >
+        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+        <Input
+          id="standard-adornment-password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) =>
+            loginStore.setState((state) => {
+              state.password = e.currentTarget.value;
+            })
+          }
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <FormControl
+        sx={{
+          m: 1,
+          width: "25ch",
+        }}
+        variant="standard"
+      >
+        <InputLabel htmlFor="standard-adornment-password">
+          Repeat password
+        </InputLabel>
+        <Input
+          id="standard-adornment-password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) =>
+            loginStore.setState((state) => {
+              state.password = e.currentTarget.value;
+            })
+          }
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(!showPassword)}
+              ></IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+    </>
+  );
+}
+
+function UsernameInput() {
+  const [showPassword, setShowPassword] = useState(false);
+  const username = loginStore((state) => state.username);
+  return (
+    <FormControl
+      sx={{
+        m: 1,
+        width: "25ch",
+      }}
+      variant="standard"
+    >
+      <InputLabel htmlFor="standard-adornment-username">Username</InputLabel>
+      <Input
+        id="standard-adornment-username"
+        type="text"
+        value={username}
+        onChange={(e) =>
+          loginStore.setState((state) => {
+            state.username = e.currentTarget.value;
+          })
+        }
+      />
+    </FormControl>
+  );
+}
+
+function ServerUrlInput() {
+  const [showPassword, setShowPassword] = useState(false);
+  const username = loginStore((state) => state.username);
+  return (
+    <FormControl
+      sx={{
+        m: 1,
+        width: "25ch",
+      }}
+      variant="standard"
+    >
+      <InputLabel htmlFor="standard-adornment-username">Server url</InputLabel>
+      <Input
+        id="standard-adornment-username"
+        type="text"
+        value={username}
+        onChange={(e) =>
+          loginStore.setState((state) => {
+            state.username = e.currentTarget.value;
+          })
+        }
       />
     </FormControl>
   );
